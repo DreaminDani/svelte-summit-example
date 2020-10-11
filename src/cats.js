599d62aaa1d4a92e3e0bf0e7dadd38d394e093ea
Daniel Sandoval <dani@chippercash.com>
2020-10-11 15:04:34 -0700

    first commit

---
+++ src/cats.js
@@ -0,0 +1,92 @@
+export const cats = [
+  {
+    "id": 0,
+    "imageLink": "https://s3.amazonaws.com/freecodecamp/funny-cat.jpg",
+    "tags": [
+      "White",
+      "Hat"
+    ]
+  },
+  {
+    "id": 1,
+    "imageLink": "https://s3.amazonaws.com/freecodecamp/grumpy-cat.jpg",
+    "altText": "A white cat with blue eyes, looking very grumpy. ",
+    "tags": [
+      "White",
+      "Grumpy"
+    ]
+  },
+  {
+    "id": 2,
+    "imageLink": "https://s3.amazonaws.com/freecodecamp/mischievous-cat.jpg",
+    "tags": [
+      "Ginger",
+      "Happy"
+    ]
+  },
+  {
+    "id": 3,
+    "imageLink": "https://cdn2.thecatapi.com/images/d11.png",
+    "tags": [
+      "Black & White",
+      "Christmas"
+    ]
+  },
+  {
+    "id": 4,
+    "imageLink": "https://cdn2.thecatapi.com/images/60q.jpg",
+    "tags": [
+      "Glasses",
+      "Business"
+    ]
+  },
+  {
+    "id": 5,
+    "imageLink": "https://cdn2.thecatapi.com/images/cbt.jpg",
+    "tags": [
+      "Grey",
+      "Hawk"
+    ]
+  },
+  {
+    "id": 6,
+    "imageLink": "https://cdn2.thecatapi.com/images/29q.jpg",
+    "tags": [
+      "Grey",
+      "Sleepy"
+    ]
+  },
+  {
+    "id": 7,
+    "imageLink": "https://cdn2.thecatapi.com/images/MTg5NjAzMw.jpg",
+    "tags": [
+      "Grey",
+      "White",
+      "Playful"
+    ]
+  },
+  {
+    "id": 8,
+    "imageLink": "https://cdn2.thecatapi.com/images/cp0.jpg",
+    "tags": [
+      "Big",
+      "Dark Grey"
+    ]
+  },
+  {
+    "id": 9,
+    "imageLink": "https://cdn2.thecatapi.com/images/RVwCfKRvR.jpg",
+    "tags": [
+      "Dark Grey",
+      "Menacing"
+    ]
+  },
+  {
+    "id": 10,
+    "imageLink": "https://cdn2.thecatapi.com/images/5ia.jpg",
+    "tags": [
+      "Space",
+      "Ginger"
+    ]
+  }
+]
